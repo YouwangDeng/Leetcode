5de101fa56f6da4ac8107c8ed73e9cc4b69f69eb
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-08 22:09:12 +0800

    Combination Sum

---
+++ Array/0039-Combination Sum.java
@@ -0,0 +1,21 @@
+//Solution 1 Reverse backtrack element can be used unlimited times
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{ 
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
