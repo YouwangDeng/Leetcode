aa259052601b9243f876c2899c7a498b2feab650
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-08 17:59:04 +0800

    Remove Duplicates from Sorted Array

---
+++ Array/0026-Remove Duplicates from Sorted Array.java
@@ -0,0 +1,8 @@
+//Solution1 O(n)
+public int removeDuplicates(int[] nums) {
+    int i = nums.length > 0 ? 1 : 0;
+    for (int n : nums)
+        if (n > nums[i-1])
+            nums[i++] = n;
+    return i;
+}
