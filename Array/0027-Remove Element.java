35bf1d886d6c4b51c054f3b8833ab296887e8c1f
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-08 18:06:11 +0800

    Remove Element

---
+++ Array/0027-Remove Element.java
@@ -0,0 +1,13 @@
+//Solution 1 
+class Solution {
+    public int removeElement(int[] A, int elem) {
+        int m = 0;    
+        for(int i = 0; i < A.length; i++) {
+           if(A[i] != elem) {
+               A[m] = A[i];
+               m++;
+           }
+        }
+        return m;
+    }
+}
