663f13345d16608ca44559cc4a79997fd5c623d0
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-10 17:15:39 +0800

    Plus One

---
+++ Array/0066-Plus One.java
@@ -0,0 +1,16 @@
+//Solution 1 Brilliant solution to "9" digit
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
