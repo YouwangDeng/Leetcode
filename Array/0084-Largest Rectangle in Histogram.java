432f00de992d23757e650eba55108925805e70f7
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-10 23:07:40 +0800

    Largest Rectangle in Histogram

---
+++ Array/0084-Largest Rectangle in Histogram.java
@@ -0,0 +1,56 @@
+//Soluton 1 Use Stack pop to calculate the area
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i <= heights.length; i++) {
+            int newHeight = i == heights.length ? 0 : heights[i];
+            if (stack.empty() || newHeight > heights[stack.peek()]) {
+                stack.push(i);
+            } else {
+                int start = stack.pop();
+                int width = stack.empty() ? i : i - 1 - stack.peek();
+                maxArea = Math.max(maxArea, heights[start] * width);
+                i--;
+            }
+        }
+        return maxArea;
+    }
+}
+//Solution 2 Much faster specific solution
+class Solution {
+    public static int largestRectangleArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int[] lessFromLeft = new int[height.length]; // idx of the first bar the left that is lower than current
+        int[] lessFromRight = new int[height.length]; // idx of the first bar the right that is lower than current
+        lessFromRight[height.length - 1] = height.length;
+        lessFromLeft[0] = -1;
+
+        for (int i = 1; i < height.length; i++) {
+            int p = i - 1;
+
+            while (p >= 0 && height[p] >= height[i]) {
+                p = lessFromLeft[p];
+            }
+            lessFromLeft[i] = p;
+        }
+
+        for (int i = height.length - 2; i >= 0; i--) {
+            int p = i + 1;
+
+            while (p < height.length && height[p] >= height[i]) {
+                p = lessFromRight[p];
+            }
+            lessFromRight[i] = p;
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            maxArea = Math.max(maxArea, height[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        }
+
+        return maxArea;
+    }
+}
