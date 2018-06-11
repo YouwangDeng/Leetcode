d5f156a88e9908fde90c3da9e9b73643f1c87af8
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-11 21:43:18 +0800

    Best Time to Buy and Sell Stock II

---
+++ Array/0122-Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,25 @@
+//Solution 1 simple strategy
+class Solution {
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        for (int i=0; i< prices.length-1; i++) {
+            if (prices[i+1]>prices[i]) total += prices[i+1]-prices[i];
+        }
+        return total;
+    }
+}
+//Solution 2
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, i = 0;
+        while (i < prices.length) {
+            // find next local minimum
+            while (i < prices.length-1 && prices[i+1] <= prices[i]) i++;
+            int min = prices[i++]; // need increment to avoid infinite loop for "[1]"
+            // find next local maximum
+            while (i < prices.length-1 && prices[i+1] >= prices[i]) i++;
+            profit += i < prices.length ? prices[i++] - min : 0;
+        }
+        return profit;
+    }
+}
