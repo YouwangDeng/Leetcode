e79fe5ec6ca132a0a2e81c2f1c93590b672364d5
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-11 23:23:53 +0800

    Longest Consecutive Sequence

---
+++ Array/0128-Longest Consecutive Sequence.java
@@ -0,0 +1,26 @@
+//Solution 1 brilliant solution 
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+
+        Set<Integer> set = new HashSet<Integer>();
+
+        for(int num: nums) set.add(num);
+        int max = 1;
+        for(int num: nums) {
+            if(set.remove(num)) {//num hasn't been visited
+                int val = num;
+                int sum = 1;
+                while(set.remove(val-1)) val--;
+                sum += num - val;
+
+                val = num;
+                while(set.remove(val+1)) val++;
+                sum += val - num;
+
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+}
