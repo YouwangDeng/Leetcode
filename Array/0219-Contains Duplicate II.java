8e67b1cbe56584ac5c0f708ba895c7fc59876659
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-12 23:11:58 +0800

    Contains Duplicate II

---
+++ Array/0219-Contains Duplicate II.java
@@ -0,0 +1,11 @@
+//Solution 1 HashSet and set.remove
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) set.remove(nums[i-k-1]);//key point
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
