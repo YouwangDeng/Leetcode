0fa16da393230bd100e342fe60a41bacae125e60
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-12 23:35:19 +0800

    Summary Ranges

---
+++ Array/0228-Summary Ranges.java
@@ -0,0 +1,18 @@
+//Solution 1 O(n)
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list=new ArrayList();
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            while(i+1<nums.length&&(nums[i+1]-nums[i])==1){
+                i++;
+            }
+            if(a!=nums[i]){
+                list.add(a+"->"+nums[i]);
+            }else{
+                list.add(a+"");
+            }
+        }
+        return list;
+    }
+}
