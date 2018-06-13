9562bc6d3d2073df3980ec353055eb1c3f66c5cc
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-13 17:36:38 +0800

    Wiggle Sort

---
+++ Array/0280-Wiggle Sort.java
@@ -0,0 +1,12 @@
+//Solution 1 odd and even and comparision O(n) TP O(1) SP
+class Solution {
+    public void wiggleSort(int[] nums) {
+        for (int i=1; i<nums.length; i++) {
+            int a = nums[i-1];
+            if ((i%2 == 1) == (a > nums[i])) {
+                nums[i-1] = nums[i];
+                nums[i] = a;
+            }
+        }
+    }
+}
