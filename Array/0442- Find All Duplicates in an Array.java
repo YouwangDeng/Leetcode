07f92baf93b5d8d32632035ad375a4dc4b0356be
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-16 16:30:52 +0800

    Find All Duplicates in an Array

---
+++ Array/0442- Find All Duplicates in an Array.java
@@ -0,0 +1,16 @@
+//Solution 1 use negative to mark used value
+class Solution {
+    // when find a number i, flip the number at position i-1 to negative. 
+    // if the number at position i-1 is already negative, i is the number that occurs twice.
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0) {
+                res.add(index+1);
+            }    
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+}
