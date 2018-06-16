c438657e2b8e58e33018ff5a335bca208176edf0
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-16 17:12:22 +0800

    Teemo Attacking

---
+++ Array/0495-Teemo Attacking.java
@@ -0,0 +1,18 @@
+//Solution 1 same idea as merge intervals
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if (timeSeries == null || timeSeries.length == 0 || duration == 0) return 0;
+        
+        int result = 0, start = timeSeries[0], end = timeSeries[0] + duration;
+        for (int i = 1; i < timeSeries.length; i++) {
+            if (timeSeries[i] > end) {
+                result += end - start;
+                start = timeSeries[i];
+            }
+            end = timeSeries[i] + duration;
+        }
+        result += end - start;
+        
+        return result;
+    }
+}
