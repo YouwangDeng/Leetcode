a5fd88c045616e69beb813486e510d553cee329d
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-17 22:59:42 +0800

    Maximum Average Subarray I

---
+++ Array/0643-Maximum Average Subarray I.java
@@ -0,0 +1,15 @@
+//Solution 1 slice solution O(n)
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        long sum = 0;
+        for (int i = 0; i < k; i++) sum += nums[i];
+        long max = sum;
+        
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
+        
+        return max / 1.0 / k;
+    }
+}
