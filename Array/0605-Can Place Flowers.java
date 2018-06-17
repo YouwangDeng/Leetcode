5577d9dd7fd3f4faccf09ff74d090fe686ecf464
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-17 17:32:15 +0800

    Can Place Flowers

---
+++ Array/0605-Can Place Flowers.java
@@ -0,0 +1,19 @@
+//Solution 1 Greedy Solution O(n)
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for(int i = 0; i < flowerbed.length && count < n; i++) {
+            if(flowerbed[i] == 0) {
+	     //get next and prev flower bed slot values. If i lies at the ends the next and prev are considered as 0. 
+               int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1]; 
+               int prev = (i == 0) ? 0 : flowerbed[i - 1];
+               if(next == 0 && prev == 0) {
+                   flowerbed[i] = 1;
+                   count++;
+               }
+            }
+        }
+        
+        return count == n;
+    }
+}
