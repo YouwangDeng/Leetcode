a1c0c206f015eab0b8e155e175c332c494e0006e
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-18 21:36:32 +0800

    Subarray Product Less Than K

---
+++ Array/0713-Subarray Product Less Than K.java
@@ -0,0 +1,16 @@
+//Solution 1 sliding window 
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0) return 0;
+        int cnt = 0;
+        int pro = 1;
+        for (int i = 0, j = 0; j < nums.length; j++) {
+            pro *= nums[j];
+            while (i <= j && pro >= k) {
+                pro /= nums[i++];
+            }
+            cnt += j - i + 1;// contiguous subarrays key point
+        }
+        return cnt;        
+    }
+}
