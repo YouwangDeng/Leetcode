c6d19c5e8e05b8b57f75ed5dcdf372a6594767bf
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-19 16:55:45 +0800

    Maximum Length of Repeated Subarray

---
+++ Array/0718-Maximum Length of Repeated Subarray.java
@@ -0,0 +1,17 @@
+//Solution 1 DP O(m*n)
+class Solution {
+    public int findLength(int[] A, int[] B) {
+        int max = 0;
+        if (A == null || B == null) return max;
+        int[][] dp = new int[A.length + 1][B.length + 1];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] == B[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    max = Math.max(max, dp[i + 1][j + 1]);
+                }
+            }
+        }
+        return max;
+    }
+}
