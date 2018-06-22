8c746a3a562b794a51085c6d1b96c4fe36a00b74
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-22 21:25:28 +0800

    Max Chunks To Make Sorted

---
+++ Array/0769-Max Chunks To Make Sorted.java
@@ -0,0 +1,14 @@
+//Solution 1 max and index O(n)
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        int count = 0, max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) {
+                count++;
+            }
+        }   
+        return count;
+    }
+}
