428466295b1c4095d80961650de72ff5c4075f78
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-22 21:49:03 +0800

    add another solution

--- Array/0769-Max Chunks To Make Sorted.java
+++ Array/0769-Max Chunks To Make Sorted.java
@@ -12,3 +12,28 @@ class Solution {
         return count;
     }
 }
+//Solution 2 maxOfLeft and minOfRight
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] maxOfLeft = new int[n];
+        int[] minOfRight = new int[n];
+
+        maxOfLeft[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            maxOfLeft[i] = Math.max(maxOfLeft[i-1], arr[i]);
+        }
+
+        minOfRight[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minOfRight[i] = Math.min(minOfRight[i + 1], arr[i]);
+        }
+
+        int res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (maxOfLeft[i] <= minOfRight[i + 1]) res++;
+        }
+
+        return res + 1;
+    }
+}
