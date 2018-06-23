396690413e4c8c704ce8dcc0afdefc5c1dffcdbd
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-23 23:01:43 +0800

    Merge k Sorted Lists

---
+++ LinkedList/0023-Merge k Sorted Lists.java
@@ -0,0 +1,67 @@
+//Solution 1 use Recursion Arrays.copyOfRange
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head=null;
+        ListNode former=null;
+        while (l1!=null&&l2!=null) {
+            if (l1.val>l2.val) {
+                if (former==null) former=l2; else former.next=l2;
+                if (head==null) head=former; else former=former.next;
+                l2=l2.next;
+            } else {
+                if (former==null) former=l1; else former.next=l1;
+                if (head==null) head=former; else former=former.next;
+                l1=l1.next;
+            }
+        }
+        if (l2!=null) l1=l2;
+        former.next=l1;
+        return head;
+        
+    }
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length==0) return null;
+        if (lists.length==1) return lists[0];
+        if (lists.length==2) return mergeTwoLists(lists[0], lists[1]);
+        return mergeTwoLists(mergeKLists(Arrays.copyOfRange(lists,0, lists.length/2)), 
+            mergeKLists(Arrays.copyOfRange(lists,lists.length/2, lists.length)));
+    }
+}
+//Solution 2 use PriorityQueue
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists==null||lists.length==0) return null;
+        
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length,new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else 
+                    return 1;
+            }
+        });
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+        
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+            
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+            
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
+    }
+}
