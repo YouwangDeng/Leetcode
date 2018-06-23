496bfe2a915a88cca2d3a1093bd3be4e3814a988
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-23 15:57:37 +0800

    Flipping an Image

---
+++ Array/0832-Flipping an Image.java
@@ -0,0 +1,11 @@
+//Solution 1 reverse and toggle same time O(n^2)
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int[] row : A)
+            for (int i = 0; i * 2 < n; i++)
+                if (row[i] == row[n - i - 1])
+                    row[i] = row[n - i - 1] ^= 1;
+        return A;
+    }
+}
