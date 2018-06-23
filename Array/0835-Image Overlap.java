f52fd58d65d47d1a0f1053ca50646f9df2ce5eb8
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-23 16:28:12 +0800

    Image Overlap

---
+++ Array/0835-Image Overlap.java
@@ -0,0 +1,16 @@
+//Solution 1 use HashMap col and row manupilation
+class Solution {
+    public int largestOverlap(int[][] A, int[][] B) {
+        int N = A.length;
+        List<Integer> LA = new ArrayList<>();
+        List<Integer> LB = new ArrayList<>();
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int i = 0; i < N * N; ++i) if (A[i / N][i % N] == 1) LA.add(i / N * 100 + i % N);
+        for (int i = 0; i < N * N; ++i) if (B[i / N][i % N] == 1) LB.add(i / N * 100 + i % N);
+        for (int i : LA) for (int j : LB)
+                count.put(i - j, count.getOrDefault(i - j, 0) + 1);
+        int res = 0;
+        for (int i : count.values()) res = Math.max(res, i);
+        return res;
+    }
+}
