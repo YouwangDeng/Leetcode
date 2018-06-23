f5bba69e671487c8254a5c66f20ed87ef58046b6
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-23 23:14:06 +0800

    Swap Nodes in Pairs

---
+++ LinkedList/0024-Swap Nodes in Pairs.java
@@ -0,0 +1,28 @@
+//Solution 1 use Recursion
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
+//Solution 2 use swap
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+}
