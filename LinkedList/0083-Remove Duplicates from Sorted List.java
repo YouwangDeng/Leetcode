4f9c8efc7190825c6157e716ceaab6550cff7dc3
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-24 23:23:13 +0800

    Remove Duplicates from Sorted List

---
+++ LinkedList/0083-Remove Duplicates from Sorted List.java
@@ -0,0 +1,24 @@
+//Solution 1 use Recursion
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null)return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+}
+//Solution 2 straight solution clear and concise
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return head;
+        
+        ListNode list = head;
+         while(list.next != null) 
+         {
+             if (list.val == list.next.val)
+                 list.next = list.next.next;
+             else 
+                 list = list.next;
+         }
+         return head;
+    }
+}
