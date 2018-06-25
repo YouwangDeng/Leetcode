78fbae2b7f269b6aa8f590674245b7a8601e5493
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-25 17:54:55 +0800

    Insertion Sort List

---
+++ LinkedList/0147-Insertion Sort List.java
@@ -0,0 +1,28 @@
+//Solution 1 pre cur and helper pointers
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+		if( head == null ){
+			return head;
+		}
+		
+		ListNode helper = new ListNode(0); //new starter of the sorted list
+		ListNode cur = head; //the node will be inserted
+		ListNode pre = helper; //insert node between pre and pre.next
+		ListNode next = null; //the next node will be inserted
+		//not the end of input list
+		while( cur != null ){
+			next = cur.next;
+            if(pre==null || pre.next==null || pre.next.val >= cur.val) pre = helper;//key point
+			//find the right place to insert
+			while( pre.next != null && pre.next.val < cur.val ){
+				pre = pre.next;
+			}
+			//insert between pre and pre.next
+			cur.next = pre.next;
+			pre.next = cur;
+			cur = next;
+		}
+		
+		return helper.next;
+	}
+}
