8956ef9dc309690e2e2f4ec44d584cf1c70ad511
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-25 22:39:17 +0800

    Split Linked List in Parts

---
+++ LinkedList/0725-Split Linked List in Parts.java
@@ -0,0 +1,20 @@
+//Solution 1 use '/' and '%' and array
+class Solution {
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        ListNode[] parts = new ListNode[k];
+        int len = 0;
+        for (ListNode node = root; node != null; node = node.next)
+            len++;
+        int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
+        ListNode node = root, prev = null;
+        for (int i = 0; node != null && i < k; i++, r--) {
+            parts[i] = node;
+            for (int j = 0; j < n + (r > 0 ? 1 : 0); j++) {
+                prev = node;
+                node = node.next;
+            }
+            prev.next = null;//key point
+        }
+        return parts;        
+    }
+}
