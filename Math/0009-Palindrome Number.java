2415c418c19446eb3464ede2c1ac5079240a6e16
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-28 21:54:33 +0800

    Palindrome Number

---
+++ Math/0009-Palindrome Number.java
@@ -0,0 +1,12 @@
+//Solution 1 use reverse number idea
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int rev = 0;
+        while (x > rev){
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == rev || x == rev / 10);
+    }
+}
