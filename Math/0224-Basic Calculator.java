58cded6207783b2288aefe690871a45bdbbd757a
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-29 20:31:54 +0800

    Basic Calculator

---
+++ Math/0224-Basic Calculator.java
@@ -0,0 +1,73 @@
+//Solution 1 use stack store sign and temperory result
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                number = 10 * number + (int)(c - '0');
+            }else if(c == '+'){
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            }else if(c == '-'){
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            }else if(c == '('){
+                //we push the result first, then sign;
+                stack.push(result);
+                stack.push(sign);
+                //reset the sign and result for the value in the parenthesis
+                sign = 1;   
+                result = 0;
+            }else if(c == ')'){
+                result += sign * number;  
+                number = 0;
+                result *= stack.pop();    //stack.pop() is the sign before the parenthesis
+                result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
+
+            }
+        }
+        if(number != 0) result += sign * number;
+        return result;
+    }
+}
+//Solution 2 use stack only to store sign faster
+class Solution {
+    public int calculate(String s) {
+        if(s == null) return 0;
+
+        int result = 0;
+        int sign = 1;
+        int num = 0;
+
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(sign);
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
+
+            } else if(c == '+' || c == '-') {
+                result += sign * num;
+                sign = stack.peek() * (c == '+' ? 1: -1); 
+                num = 0;
+
+            } else if(c == '(') {
+                stack.push(sign);
+
+            } else if(c == ')') {
+                stack.pop();
+            }
+        }
+
+        result += sign * num;
+        return result;
+    }
+}
