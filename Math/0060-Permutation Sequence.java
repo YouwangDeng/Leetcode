4887ac234f6174f857ed9d208ac85f6dace0114d
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-29 12:21:42 +0800

    Permutation Sequence

---
+++ Math/0060-Permutation Sequence.java
@@ -0,0 +1,19 @@
+//Solution 1 find the pattern use iteration
+class Solution {
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            num.add(i);
+        }
+        for (int i = 0, l = k - 1; i < n; i++) {
+            fact /= (n - i);
+            int index = (l / fact);
+            sb.append(num.remove(index));
+            l -= index * fact;
+        }
+        return sb.toString();
+    }
+}
