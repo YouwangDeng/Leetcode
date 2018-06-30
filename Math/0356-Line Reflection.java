29e2898aa0029eccf2362452eac1b6a47ea74d91
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-06-30 21:03:46 +0800

    Line Reflection

---
+++ Math/0356-Line Reflection.java
@@ -0,0 +1,23 @@
+//Solution 1 defination of reflect
+class Solution {
+    public boolean isReflected(int[][] points) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        HashSet<String> set = new HashSet<>();
+        for(int[] p:points){
+            max = Math.max(max,p[0]);
+            min = Math.min(min,p[0]);
+            String str = p[0] + "a" + p[1];
+            set.add(str);
+        }
+        int sum = max+min;
+        for(int[] p:points){
+            //int[] arr = {sum-p[0],p[1]};
+            String str = (sum-p[0]) + "a" + p[1];
+            if( !set.contains(str))
+                return false;
+
+        }
+        return true;
+    }
+}
