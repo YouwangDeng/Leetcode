efd00ce9dbd224e7f465bceb8ec9da019d087b0b
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-01 18:30:38 +0800

    Minimum Moves to Equal Array Elements II

---
+++ Math/0462-Minimum Moves to Equal Array Elements II.java
@@ -0,0 +1,14 @@
+//Solution 1 meet point solution median point
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length-1;
+        int count = 0;
+        while(i < j){
+            count += nums[j]-nums[i];
+            i++;
+            j--;
+        }
+        return count;
+    }
+}
