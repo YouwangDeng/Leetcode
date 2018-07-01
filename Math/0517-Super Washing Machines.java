e45e1c5a150fe6f847d05a6e03465caf4dd69291
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-01 23:51:25 +0800

    Super Washing Machines

---
+++ Math/0517-Super Washing Machines.java
@@ -0,0 +1,14 @@
+//Solution 1 two cases sum give and lose
+class Solution {
+    public int findMinMoves(int[] machines) {
+        int total = 0; 
+        for(int i: machines) total+=i;
+        if(total%machines.length!=0) return -1;
+        int avg = total/machines.length, cnt = 0, max = 0;
+        for(int load: machines){
+            cnt += load-avg; //load-avg is "gain/lose"
+            max = Math.max(Math.max(max, Math.abs(cnt)), load-avg);
+        }
+        return max;
+    }
+}
