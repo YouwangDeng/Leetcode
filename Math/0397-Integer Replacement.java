c210b185fe702c2c052fae3e5eb9a921de4b3ec6
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-01 14:51:57 +0800

    Integer Replacement

---
+++ Math/0397-Integer Replacement.java
@@ -0,0 +1,17 @@
+//Solution 1 bit manipulation
+class Solution {
+    public int integerReplacement(int n) {
+        int c = 0;
+        while (n != 1) {
+            if ((n & 1) == 0) {
+                n >>>= 1;
+            } else if (n == 3 || ((n >>> 1) & 1) == 0) {
+                --n;
+            } else {
+                ++n;
+            }
+            ++c;
+        }
+        return c;
+    }
+}
