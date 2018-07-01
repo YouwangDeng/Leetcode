4b89760c221219e6d4427f311aab94c27706b37a
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-01 17:32:54 +0800

    Add Strings

---
+++ Math/0415-Add Strings.java
@@ -0,0 +1,14 @@
+//Solution 1 from the back to the front and use carry
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
+            int x = i < 0 ? 0 : num1.charAt(i) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+        }
+        return sb.reverse().toString();
+    }
+}
