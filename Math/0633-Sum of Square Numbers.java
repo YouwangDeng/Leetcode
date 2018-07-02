01eead4feed5a1c587519983911a974d313e4771
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-02 16:42:32 +0800

    Sum of Square Numbers

---
+++ Math/0633-Sum of Square Numbers.java
@@ -0,0 +1,20 @@
+//Solution 1 two pointers solution
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        if (c < 0) {
+            return false;
+        }
+        int left = 0, right = (int)Math.sqrt(c);
+        while (left <= right) {
+            int cur = left * left + right * right;
+            if (cur < c) {
+                left++;
+            } else if (cur > c) {
+                right--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
