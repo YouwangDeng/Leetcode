f1d7017f6c3e8a82342d9f71f93440564c965053
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-02 16:19:31 +0800

    Range Addition II

---
+++ Math/0598-Range Addition II.java
@@ -0,0 +1,16 @@
+//Solution 1 find the minimum
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        if (ops == null || ops.length == 0) {
+            return m * n;
+        }
+        
+        int row = Integer.MAX_VALUE, col = Integer.MAX_VALUE;
+        for(int[] op : ops) {
+            row = Math.min(row, op[0]);
+            col = Math.min(col, op[1]);
+        }
+        
+        return row * col;
+    }
+}
