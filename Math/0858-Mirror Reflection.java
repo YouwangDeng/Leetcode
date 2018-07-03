e4e7b01295270e0db5d463f7ea60164dd586df28
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-03 23:11:26 +0800

    Mirror Reflection

---
+++ Math/0858-Mirror Reflection.java
@@ -0,0 +1,16 @@
+//Solution 1 reflection simulation
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0) {
+            p /= 2;
+            q /= 2;
+        }
+        if (p % 2 == 0) {
+            return 2;
+        } else if (q % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
