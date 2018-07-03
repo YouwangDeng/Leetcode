475c82342cae3334c11d9f659185bbb7686888ae
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-03 21:35:41 +0800

    Largest Triangle Area

---
+++ Math/0812-Largest Triangle Area.java
@@ -0,0 +1,11 @@
+//Solution 1 math solution
+class Solution {
+    public double largestTriangleArea(int[][] p) {
+        double res = 0;
+        for (int[] i: p)
+            for (int[] j: p)
+                for (int[] k: p)
+            res = Math.max(res, 0.5 * Math.abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1]- j[0] * i[1] - k[0] * j[1] - i[0] * k[1]));
+        return res;
+    }
+}
