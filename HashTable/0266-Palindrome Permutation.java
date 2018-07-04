4ee044922f9848707cd4606fab0761dfbfad123e
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-04 21:56:07 +0800

    Palindrome Permutation

---
+++ HashTable/0266-Palindrome Permutation.java
@@ -0,0 +1,13 @@
+//Solution 1 use HashSet
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set=new HashSet<Character>();
+        for(int i=0; i<s.length(); ++i){
+            if (!set.contains(s.charAt(i)))
+                set.add(s.charAt(i));
+            else 
+                set.remove(s.charAt(i));
+        }
+        return set.size()==0 || set.size()==1;
+    }
+}
