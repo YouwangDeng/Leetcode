a5bc603be42f46c1fb8b609c19d3065897ebbc14
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-06 23:07:38 +0800

    Distribute Candies

---
+++ HashTable/0575- Distribute Candies.java
@@ -0,0 +1,8 @@
+//Solution 1 use HashSet
+public class Solution {
+    public int distributeCandies(int[] candies) {
+        Set<Integer> kinds = new HashSet<>();
+        for (int candy : candies) kinds.add(candy);
+        return kinds.size() >= candies.length / 2 ? candies.length / 2 : kinds.size();
+    }
+}
