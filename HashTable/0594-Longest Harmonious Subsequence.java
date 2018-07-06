12278c721d5c6bfe70ad02eb21f90963a087628e
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-06 23:14:29 +0800

    Longest Harmonious Subsequence

---
+++ HashTable/0594-Longest Harmonious Subsequence.java
@@ -0,0 +1,16 @@
+//Solution 1 use HashMap to store frequency
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Long, Integer> map = new HashMap<>();
+        for (long num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int result = 0;
+        for (long key : map.keySet()) {//key point use long to avoid overflow
+            if (map.containsKey(key + 1)) {
+                result = Math.max(result, map.get(key + 1) + map.get(key));
+            }
+        }
+        return result;
+    }
+}
