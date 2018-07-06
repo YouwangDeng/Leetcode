ce263900699520ccc71492ddbf2102c56831dffe
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-06 23:47:40 +0800

    Minimum Index Sum of Two Lists

---
+++ HashTable/0599-Minimum Index Sum of Two Lists.java
@@ -0,0 +1,17 @@
+//Solution 1 use HashMap
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> res = new LinkedList<>();
+        int minSum = Integer.MAX_VALUE;
+        for (int i=0;i<list1.length;i++) map.put(list1[i], i);
+        for (int i=0;i<list2.length;i++) {
+            Integer j = map.get(list2[i]);
+            if (j != null && i + j <= minSum) {
+                if (i + j < minSum) { res.clear(); minSum = i+j; }//key point and it is a trick
+                res.add(list2[i]);
+            }
+        }
+        return res.toArray(new String[0]);
+    }
+}
