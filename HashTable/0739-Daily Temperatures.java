0910fc345df9aeb926d75ebcfa933a4284712b5c
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-07 22:30:17 +0800

    Daily Temperatures

---
+++ HashTable/0739-Daily Temperatures.java
@@ -0,0 +1,31 @@
+//Solution 1 use Stack
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                ret[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return ret;
+    }
+}
+//Solution 2 use array to simulate stack
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(top > -1 && temperatures[i] > temperatures[stack[top]]) {
+                int idx = stack[top--];
+                ret[idx] = i - idx;
+            }
+            stack[++top] = i;
+        }
+        return ret;
+    }
+}
