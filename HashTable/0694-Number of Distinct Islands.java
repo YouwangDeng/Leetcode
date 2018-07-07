9c4b7f5f9eb36d2fd9d9bdd19a8ed976cfcd344a
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-07 19:43:06 +0800

    Number of Distinct Islands

---
+++ HashTable/0694-Number of Distinct Islands.java
@@ -0,0 +1,27 @@
+//Solution 1 use dfs and HashSet
+class Solution {
+    public int numDistinctIslands(int[][] grid) {
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] != 0) {
+                    StringBuilder sb = new StringBuilder();
+                    dfs(grid, i, j, sb, "o"); // origin
+                    grid[i][j] = 0;
+                    set.add(sb.toString());
+                }
+            }
+        }
+        return set.size();
+    }
+    private void dfs(int[][] grid, int i, int j, StringBuilder sb, String dir) {
+        if(i < 0 || i == grid.length || j < 0 || j == grid[i].length || grid[i][j] == 0) return;
+        sb.append(dir);
+        grid[i][j] = 0;
+        dfs(grid, i-1, j, sb, "u"); // up
+        dfs(grid, i+1, j, sb, "d"); // down
+        dfs(grid, i, j-1, sb, "l"); // left
+        dfs(grid, i, j+1, sb, "r"); // right
+        sb.append("b"); // back key point must have
+    }
+}
