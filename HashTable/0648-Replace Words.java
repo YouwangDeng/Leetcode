90ca47330f0bc0593402dfada03cdef4540b191a
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-07 18:16:49 +0800

    Replace Words

---
+++ HashTable/0648-Replace Words.java
@@ -0,0 +1,84 @@
+//Solution 1 use HashSet
+public class Solution {
+    public String replaceWords(List<String> dict, String sentence) {
+        if (dict == null || dict.size() == 0) return sentence;
+        
+        Set<String> set = new HashSet<>();
+        for (String s : dict) set.add(s);
+        
+        StringBuilder sb = new StringBuilder();
+        String[] words = sentence.split("\\s+");
+        
+        for (String word : words) {
+            String prefix = "";
+            for (int i = 1; i <= word.length(); i++) {
+                prefix = word.substring(0, i);
+                if (set.contains(prefix)) break;
+            }
+            sb.append(" " + prefix);
+        }
+        
+        return sb.deleteCharAt(0).toString();
+    }
+}
+//Solution 2 use Trie data structure much faster
+public class Solution {
+    public String replaceWords(List<String> dict, String sentence) {
+        String[] tokens = sentence.split(" ");
+        TrieNode trie = buildTrie(dict);
+        return replaceWords(tokens, trie);
+    }
+
+    private String replaceWords(String[] tokens, TrieNode root) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String token : tokens) {
+            stringBuilder.append(getShortestReplacement(token, root));
+            stringBuilder.append(" ");
+        }
+        return stringBuilder.substring(0, stringBuilder.length()-1);
+    }
+
+    private String getShortestReplacement(String token, final TrieNode root) {
+        TrieNode temp = root;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : token.toCharArray()) {
+            stringBuilder.append(c);
+            if (temp.children[c - 'a'] != null) {
+                if (temp.children[c - 'a'].isWord) {
+                    return stringBuilder.toString();
+                }
+                temp = temp.children[c - 'a'];
+            } else {
+                return token;
+            }
+        }
+        return token;
+    }
+
+    private TrieNode buildTrie(List<String> dict) {
+        TrieNode root = new TrieNode(' ');
+        for (String word : dict) {
+            TrieNode temp = root;
+            for (char c : word.toCharArray()) {
+                if (temp.children[c - 'a'] == null) {
+                    temp.children[c - 'a'] = new TrieNode(c);
+                }
+                temp = temp.children[c - 'a'];
+            }
+            temp.isWord = true;
+        }
+        return root;
+    }
+
+    public class TrieNode {
+        char val;
+        TrieNode[] children;
+        boolean isWord;
+
+        public TrieNode(char val) {
+            this.val = val;
+            this.children = new TrieNode[26];
+            this.isWord = false;
+        }
+    }
+}
