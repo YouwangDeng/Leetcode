5a60318356efecde996e8068f082c8046887bd3c
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-07 21:29:55 +0800

    Longest Word in Dictionary

---
+++ HashTable/0720-Longest Word in Dictionary.java
@@ -0,0 +1,15 @@
+//Solution 1 use HashSet
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        Set<String> built = new HashSet<String>();
+        String res = "";
+        for (String w : words) {
+            if (w.length() == 1 || built.contains(w.substring(0, w.length() - 1))) {
+                res = w.length() > res.length() ? w : res;
+                built.add(w);
+            }
+        }
+        return res;
+    }
+}
