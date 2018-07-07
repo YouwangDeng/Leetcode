e142721a2d4f75614a6cdfc79caacab4963c5c6c
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-07 18:44:18 +0800

    Employee Importance

---
+++ HashTable/0690-Employee Importance.java
@@ -0,0 +1,32 @@
+//Solution 1 use HashMap
+/*
+// Employee info
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+*/
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        int total = 0;
+        Map<Integer, Employee> map = new HashMap<>();
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+        Queue<Employee> queue = new LinkedList<>();
+        queue.offer(map.get(id));
+        while (!queue.isEmpty()) {
+            Employee current = queue.poll();
+            total += current.importance;
+            for (int subordinate : current.subordinates) {
+                queue.offer(map.get(subordinate));
+            }
+        }
+        return total;
+    }
+}
