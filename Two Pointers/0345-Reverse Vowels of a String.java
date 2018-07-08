950ed6a019723f2bdc72f1c636811d7d2a2eefd5
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-08 17:25:22 +0800

    Reverse Vowels of a String

---
+++ Two Pointers/0345-Reverse Vowels of a String.java
@@ -0,0 +1,28 @@
+//Solution 1 standard two pointers solution String contains method must be string
+public class Solution {
+    public String reverseVowels(String s) {
+        if(s == null || s.length()==0) return s;
+        String vowels = "aeiouAEIOU";
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = s.length()-1;
+        while(start<end){
+
+            while(start<end && !vowels.contains(chars[start]+"")){//The .contains method is checking for string within a string. By adding the double quotes to the char, it is making that char into a string.
+                start++;
+            }
+
+            while(start<end && !vowels.contains(chars[end]+"")){
+                end--;
+            }
+
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+
+            start++;
+            end--;
+        }
+        return new String(chars);
+    }
+}
