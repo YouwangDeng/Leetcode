36eef0194bafdf97803a31cfd541992e4242a94e
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-08 16:29:56 +0800

    Valid Palindrome

---
+++ Two Pointers/0125-Valid Palindrome.java
@@ -0,0 +1,27 @@
+//Solution 1 use head and tail two pointers
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+        	cHead = s.charAt(head);
+        	cTail = s.charAt(tail);
+        	if (!Character.isLetterOrDigit(cHead)) {
+        		head++;
+        	} else if(!Character.isLetterOrDigit(cTail)) {
+        		tail--;
+        	} else {
+        		if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+        			return false;
+        		}
+        		head++;
+        		tail--;
+        	}
+        }
+        
+        return true;
+    }
+}
