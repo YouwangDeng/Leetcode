1a7b4da9510a9317fd5159579001cc9b7f923114
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-08 21:47:15 +0800

    Partition Labels

---
+++ Two Pointers/0763-Partition Labels.java
@@ -0,0 +1,25 @@
+//Solution 1 use a char array and two pointers last and start to record the substring position
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        if(S == null || S.length() == 0){
+            return null;
+        }
+        List<Integer> list = new ArrayList<>();
+        int[] map = new int[26];  // record the last index of the each char
+
+        for(int i = 0; i < S.length(); i++){
+            map[S.charAt(i)-'a'] = i;
+        }
+        // record the end index of the current sub string
+        int last = 0;
+        int start = 0;
+        for(int i = 0; i < S.length(); i++){
+            last = Math.max(last, map[S.charAt(i)-'a']);
+            if(last == i){
+                list.add(last - start + 1);
+                start = last + 1;
+            }
+        }
+        return list;
+    }
+}
