b6846e5223fdcd38967a73ddef76d165a09801ca
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-10 13:38:34 +0800

    Valid Parentheses

---
+++ String/0020-Valid Parentheses.java
@@ -0,0 +1,17 @@
+//Solution 1 use Stack
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');//key point
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)//key point
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
