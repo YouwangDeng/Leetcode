f132ac7e219aac53b66615a74301d3f342b23d35
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-11 19:29:35 +0800

    Read N Characters Given Read4 II - Call multiple times

---
+++ String/0158-Read N Characters Given Read4 II - Call multiple times.java
@@ -0,0 +1,28 @@
+//Solution 1 keep a buffer pointer
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    private int buffPtr = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            if (buffPtr == 0) {
+                buffCnt = read4(buff);
+            }
+            if (buffCnt == 0) break;
+            while (ptr < n && buffPtr < buffCnt) {
+                buf[ptr++] = buff[buffPtr++];
+            }
+            if (buffPtr >= buffCnt) buffPtr = 0;
+        }
+        return ptr;
+    }
+}
