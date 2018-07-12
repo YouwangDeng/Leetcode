e958e8f84819135d46bc344f6362b1654a162019
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-12 20:33:05 +0800

    Word Abbreviation

---
+++ String/0527-Word Abbreviation.java
@@ -0,0 +1,34 @@
+//Solution 1 use HashSet and StringBuilder
+class Solution {
+    public List<String> wordsAbbreviation(List<String> dict) {
+        int len=dict.size();
+        String[] ans=new String[len];
+        int[] prefix=new int[len];
+        for (int i=0;i<len;i++) {
+            prefix[i]=1;
+            ans[i]=makeAbbr(dict.get(i), 1); // make abbreviation for each string
+        }
+        for (int i=0;i<len;i++) {
+            while (true) {
+                HashSet<Integer> set=new HashSet<>();
+                for (int j=i+1;j<len;j++) {
+                    if (ans[j].equals(ans[i])) set.add(j); // check all strings with the same abbreviation
+                }
+                if (set.isEmpty()) break;
+                set.add(i);
+                for (int k: set) 
+                    ans[k]=makeAbbr(dict.get(k), ++prefix[k]); // increase the prefix
+            }
+        }
+        return Arrays.asList(ans);
+    }
+
+    private String makeAbbr(String s, int k) {
+        if (k>=s.length()-2) return s;
+        StringBuilder builder=new StringBuilder();
+        builder.append(s.substring(0, k));
+        builder.append(s.length()-1-k);
+        builder.append(s.charAt(s.length()-1));
+        return builder.toString();
+    }
+}
