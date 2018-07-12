7519cbe2173b8af5b24eb2cbffafb0cbf0434c24
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-12 21:25:20 +0800

    Reverse String II

---
+++ String/0541-Reverse String II.java
@@ -0,0 +1,21 @@
+//Solution 1 use String.valueOf method and reverse seperated function
+public class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        while(i < n) {
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i += 2 * k;
+        }
+        return String.valueOf(arr);
+    }
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+}
