f44b49fa8cd77094777acc1bbb85f259fdb5ea50
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-12 18:52:23 +0800

    Detect Capital

---
+++ String/0520-Detect Capital.java
@@ -0,0 +1,8 @@
+//Solution 1 use ASCII table
+public class Solution {
+    public boolean detectCapitalUse(String word) {
+        int cnt = 0;
+        for(char c: word.toCharArray()) if('Z' - c >= 0) cnt++;
+        return ((cnt==0 || cnt==word.length()) || (cnt==1 && 'Z' - word.charAt(0)>=0));
+    }
+}
