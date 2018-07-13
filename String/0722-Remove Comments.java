58d87f72fbdcae3b48926b8d576a9fd69fdb41b1
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-13 21:53:33 +0800

    Remove Comments

---
+++ String/0722-Remove Comments.java
@@ -0,0 +1,33 @@
+//Solution 1 detailed solution use flag "mode"
+class Solution {
+    public List<String> removeComments(String[] source) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();     
+        boolean mode = false;
+        for (String s : source) {
+            for (int i = 0; i < s.length(); i++) {
+                if (mode) {
+                    if (s.charAt(i) == '*' && i < s.length() - 1 && s.charAt(i + 1) == '/') {
+                        mode = false;
+                        i++;        //skip '/' on next iteration of i
+                    }
+                }
+                else {
+                    if (s.charAt(i) == '/' && i < s.length() - 1 && s.charAt(i + 1) == '/') {
+                        break;      //ignore remaining characters on line s
+                    }
+                    else if (s.charAt(i) == '/' && i < s.length() - 1 && s.charAt(i + 1) == '*') {
+                        mode = true;
+                        i++;           //skip '*' on next iteration of i
+                    }
+                    else    sb.append(s.charAt(i));     //not a comment
+                }
+            }
+            if (!mode && sb.length() > 0) {
+                res.add(sb.toString());
+                sb = new StringBuilder();   //reset for next line of source code
+            }
+        }
+        return res;
+    }
+}
