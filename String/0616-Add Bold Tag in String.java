ee58d148d0454b480ba736e75f02f4596ec29a8b
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-13 13:58:40 +0800

    Add Bold Tag in String

---
+++ String/0616-Add Bold Tag in String.java
@@ -0,0 +1,28 @@
+//Solution 1 use startsWith method and StringBuider
+public class Solution {
+    public String addBoldTag(String s, String[] dict) {
+        boolean[] bold = new boolean[s.length()];//key point
+        for (int i = 0, end = 0; i < s.length(); i++) {
+            for (String word : dict) {
+                if (s.startsWith(word, i)) {
+                    end = Math.max(end, i + word.length());
+                }
+            }
+            bold[i] = end > i;
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!bold[i]) {
+                result.append(s.charAt(i));
+                continue;
+            }
+            int j = i;
+            while (j < s.length() && bold[j]) j++;
+            result.append("<b>" + s.substring(i, j) + "</b>");
+            i = j - 1;
+        }
+        
+        return result.toString();
+    }
+}
