fb1003284133e7d3c3d911cae5102d678ee5a054
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-13 17:00:18 +0800

    Valid Parenthesis String

---
+++ String/0678-Valid Parenthesis String.java
@@ -0,0 +1,27 @@
+//Solution 1 use low and high to mark the possible numbers
+class Solution {
+	public boolean checkValidString(String s) {
+        int low = 0;
+        int high = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                low++;
+                high++;
+            } else if (s.charAt(i) == ')') {
+                if (low > 0) {
+                    low--;
+                }
+                high--;
+            } else {
+                if (low > 0) {
+                    low--;
+                }
+                high++;
+            }
+            if (high < 0) {
+                return false;
+            }
+        }
+        return low == 0;
+    }
+}
