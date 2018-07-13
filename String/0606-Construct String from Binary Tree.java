dc38f41f807ddeb4db0fb3bf6f2f00a05aad7bc5
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-13 13:22:53 +0800

    Construct String from Binary Tree

---
+++ String/0606-Construct String from Binary Tree.java
@@ -0,0 +1,25 @@
+//Solution 1 use recursion clear solution 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public String tree2str(TreeNode t) {
+        if (t == null) return "";
+        
+        String result = t.val + "";
+        
+        String left = tree2str(t.left);
+        String right = tree2str(t.right);
+        
+        if (left == "" && right == "") return result;
+        if (left == "") return result + "()" + "(" + right + ")";
+        if (right == "") return result + "(" + left + ")";
+        return result + "(" + left + ")" + "(" + right + ")";
+    }
+}
