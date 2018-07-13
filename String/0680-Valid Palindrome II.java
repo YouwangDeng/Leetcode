de55e1c73d3271d5bb02ee2f202be35afa5181b2
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-13 17:12:16 +0800

    Valid Palindrome II

---
+++ String/0680-Valid Palindrome II.java
@@ -0,0 +1,15 @@
+//Solution 1 use charAt method and "++" and "--" trick
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = -1, r = s.length();
+        while (++l < --r) 
+            if (s.charAt(l) != s.charAt(r)) return isPalindromic(s, l, r+1) || isPalindromic(s, l-1, r);
+        return true;
+    }
+
+    public boolean isPalindromic(String s, int l, int r) {
+        while (++l < --r) 
+            if (s.charAt(l) != s.charAt(r)) return false;
+        return true;
+    }
+}
