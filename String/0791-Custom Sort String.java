0dbb5adf2dd0348b503d48b3ebff2ec495c1a67b
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-14 19:13:33 +0800

    Custom Sort String

---
+++ String/0791-Custom Sort String.java
@@ -0,0 +1,15 @@
+//Solution 1 clear solution  char array trick
+class Solution {
+    public String customSortString(String S, String T) {
+        int[] count = new int[26];
+        for (char c : T.toCharArray()) { ++count[c - 'a']; }  // count each char in T.
+        StringBuilder sb = new StringBuilder();
+        for (char c : S.toCharArray()) {                            
+            while (count[c - 'a']-- > 0) { sb.append(c); }    // sort chars both in T and S by the order of S.
+        }
+        for (char c = 'a'; c <= 'z'; ++c) {
+            while (count[c - 'a']-- > 0) { sb.append(c); }   // group chars in T but not in S.
+        }
+        return sb.toString();
+   }
+}
