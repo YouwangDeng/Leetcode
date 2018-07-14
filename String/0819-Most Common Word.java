7cd98db1484176a9288cb940036d41aa4dc59cab
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-14 21:00:01 +0800

    Most Common Word

---
+++ String/0819-Most Common Word.java
@@ -0,0 +1,10 @@
+//Solution 1 use Array.asList, replaceAll, toLowerCase, split, Collections.max, Map.Entry.comparingByValue methods
+class Solution {
+    public String mostCommonWord(String p, String[] banned) {
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> count = new HashMap<>();
+        String[] words = p.replaceAll("\\pP" , "").toLowerCase().split("\\s+");
+        for (String w : words) if (!ban.contains(w)) count.put(w, count.getOrDefault(w, 0) + 1);
+        return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
