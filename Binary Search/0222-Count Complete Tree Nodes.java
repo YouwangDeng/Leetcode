37272e1ffafecae2f3e9148dfa7cb3cf32c5b1c9
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-15 12:43:26 +0800

    Count Complete Tree Nodes

---
+++ Binary Search/0222-Count Complete Tree Nodes.java
@@ -0,0 +1,41 @@
+//Solution 1 recursion solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int height(TreeNode root) {
+        return root == null ? -1 : 1 + height(root.left);
+    }
+    public int countNodes(TreeNode root) {
+        int h = height(root);
+        return h < 0 ? 0 :
+               height(root.right) == h-1 ? (1 << h) + countNodes(root.right)
+                                         : (1 << h-1) + countNodes(root.left);
+    }
+}
+//Solution 2 iteration solution faster
+class Solution {
+    int height(TreeNode root) {
+        return root == null ? -1 : 1 + height(root.left);
+    }
+    public int countNodes(TreeNode root) {
+        int nodes = 0, h = height(root);
+        while (root != null) {
+            if (height(root.right) == h - 1) {
+                nodes += 1 << h;
+                root = root.right;
+            } else {
+                nodes += 1 << h-1;
+                root = root.left;
+            }
+            h--;
+        }
+        return nodes;
+    }
+}
