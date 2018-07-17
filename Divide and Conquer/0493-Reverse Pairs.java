19e93b5ce9799891701b82a818d8290966b57d62
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-17 22:00:41 +0800

    Reverse Pairs

---
+++ Divide and Conquer/0493-Reverse Pairs.java
@@ -0,0 +1,17 @@
+//Solution 1 merge sort solution
+public class Solution {
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length-1);
+    }
+    private int mergeSort(int[] nums, int s, int e){
+        if(s>=e) return 0; 
+        int mid = s + (e-s)/2; 
+        int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e); 
+        for(int i = s, j = mid+1; i<=mid; i++){
+            while(j<=e && nums[i]/2.0 > nums[j]) j++; 
+            cnt += j-(mid+1); 
+        }
+        Arrays.sort(nums, s, e+1); 
+        return cnt; 
+    }
+}
