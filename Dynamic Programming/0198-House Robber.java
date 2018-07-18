0b2dd6996ff99537cf4699fa0d6d7d8d978e5c79
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-18 17:10:52 +0800

    House Robber

---
+++ Dynamic Programming/0198-House Robber.java
@@ -0,0 +1,13 @@
+//Solution 1 use binary DP solution
+class Solution {
+    public int rob(int[] num) {
+        int prevNo = 0;
+        int prevYes = 0;
+        for (int n : num) {
+            int temp = prevNo;
+            prevNo = Math.max(prevNo, prevYes);
+            prevYes = n + temp;
+        }
+        return Math.max(prevNo, prevYes);
+    }
+}
