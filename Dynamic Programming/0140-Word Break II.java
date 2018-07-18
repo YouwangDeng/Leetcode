56b141191cd1899519c753e97e04d04caf5c284e
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-18 12:46:01 +0800

    Word Break II

---
+++ Dynamic Programming/0140-Word Break II.java
@@ -0,0 +1,29 @@
+//Solution 1 use HashMap to store temp values recursion solution
+public class Solution {
+    HashMap<String,List<String>> map = new HashMap<String,List<String>>();
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        List<String> res = new ArrayList<String>();
+        if(s == null || s.length() == 0) {
+            return res;
+        }
+        if(map.containsKey(s)) {
+            return map.get(s);
+        }
+        if(wordDict.contains(s)) {
+            res.add(s);
+        }
+        for(int i = 1 ; i < s.length() ; i++) {
+            String t = s.substring(i);
+            if(wordDict.contains(t)) {
+                List<String> temp = wordBreak(s.substring(0 , i) , wordDict);
+                if(temp.size() != 0) {
+                    for(int j = 0 ; j < temp.size() ; j++) {
+                        res.add(temp.get(j) + " " + t);
+                    }
+                }
+            }
+        }
+        map.put(s , res);
+        return res;
+    }
+}
