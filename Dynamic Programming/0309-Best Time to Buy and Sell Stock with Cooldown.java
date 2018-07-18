eaa357179c56ceccd8d46850ab1287d8b11482c7
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-18 23:15:41 +0800

    Best Time to Buy and Sell Stock with Cooldown

---
+++ Dynamic Programming/0309-Best Time to Buy and Sell Stock with Cooldown.java
@@ -0,0 +1,13 @@
+//Solution 1 use Math DP solution
+class Solution {
+	public int maxProfit(int[] prices) {
+	    int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
+	    for (int price : prices) {
+	        prev_buy = buy;
+	        buy = Math.max(prev_sell - price, prev_buy);
+	        prev_sell = sell;
+	        sell = Math.max(prev_buy + price, prev_sell);
+	    }
+	    return sell;
+	}
+}
