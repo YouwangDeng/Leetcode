cf95d8f546986bcc4fc794f597ae6d4585cefee0
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-19 13:03:33 +0800

    Counting Bits

---
+++ Dynamic Programming/0338-Counting Bits.java
@@ -0,0 +1,8 @@
+//Solution 1 bit manipulation DP solution
+class Solution {
+    public int[] countBits(int num) {
+        int[] f = new int[num + 1];
+        for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+        return f;
+    }
+}
