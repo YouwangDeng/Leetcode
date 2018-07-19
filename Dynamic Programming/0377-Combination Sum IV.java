d5173821ed5c7cd9379476cb8d3ca6fa24d85ec2
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-19 19:56:47 +0800

    Combination Sum IV

---
+++ Dynamic Programming/0377-Combination Sum IV.java
@@ -0,0 +1,40 @@
+//Solution 1 recursion top-down DP solution
+class Solution {
+    private int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return helper(nums, target);
+    }
+
+    private int helper(int[] nums, int target) {
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) {
+                res += helper(nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return res;
+    }
+}
+//Solution 2 bottom-up iteration DP solution
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] comb = new int[target + 1];
+        comb[0] = 1;
+        for (int i = 1; i < comb.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    comb[i] += comb[i - nums[j]];
+                }
+            }
+        }
+        return comb[target];
+    }
+}
