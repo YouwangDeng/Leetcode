efb52542465284964b57259022542ff6c6077d2e
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-20 18:11:30 +0800

    Remove Boxes

---
+++ Dynamic Programming/0546-Remove Boxes.java
@@ -0,0 +1,25 @@
+//Solution 1 top-down recursive DP solution
+class Solution {
+    public int removeBoxes(int[] boxes) {
+        int n = boxes.length;
+        int[][][] dp = new int[n][n][n];
+        return removeBoxesSub(boxes, 0, n - 1, 0, dp);
+    }
+
+    private int removeBoxesSub(int[] boxes, int i, int j, int k, int[][][] dp) {
+        if (i > j) return 0;
+        if (dp[i][j][k] > 0) return dp[i][j][k];
+
+        for (; i + 1 <= j && boxes[i + 1] == boxes[i]; i++, k++); // optimization: all boxes of the same color counted continuously from the first box should be grouped together
+        int res = (k + 1) * (k + 1) + removeBoxesSub(boxes, i + 1, j, 0, dp);
+
+        for (int m = i + 1; m <= j; m++) {
+            if (boxes[i] == boxes[m]) {
+                res = Math.max(res, removeBoxesSub(boxes, i + 1, m - 1, 0, dp) + removeBoxesSub(boxes, m, j, k + 1, dp));
+            }
+        }
+
+        dp[i][j][k] = res;
+        return res;
+    }
+}
