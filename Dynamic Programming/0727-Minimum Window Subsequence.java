9d029320fcb1dd8ebeab36866ff9bb5ebd6482d5
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-21 23:32:47 +0800

    Minimum Window Subsequence

---
+++ Dynamic Programming/0727-Minimum Window Subsequence.java
@@ -0,0 +1,30 @@
+//Solution 1 substring and subsquence DP solution
+class Solution {
+    public String minWindow(String S, String T) {
+        int m = T.length(), n = S.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j + 1;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (T.charAt(i - 1) == S.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+
+        int start = 0, len = n + 1;
+        for (int j = 1; j <= n; j++) {
+            if (dp[m][j] != 0) {
+                if (j - dp[m][j] + 1 < len) {
+                    start = dp[m][j] - 1;
+                    len = j - dp[m][j] + 1;
+                }
+            }
+        }
+        return len == n + 1 ? "" : S.substring(start, start + len);
+    }
+}
