ffbaa8458f7fa1df9a8b028bb78ab790c945c8ad
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-21 20:38:31 +0800

    Number of Longest Increasing Subsequence

---
+++ Dynamic Programming/0673-Number of Longest Increasing Subsequence.java
@@ -0,0 +1,25 @@
+//Solution 1 use len and count DP solution
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length, res = 0, max_len = 0;
+        int[] len =  new int[n], cnt = new int[n];
+        for(int i = 0; i<n; i++){
+            len[i] = cnt[i] = 1;
+            for(int j = 0; j <i ; j++){
+                if(nums[i] > nums[j]){
+                    if(len[i] == len[j] + 1)cnt[i] += cnt[j];
+                    if(len[i] < len[j] + 1){
+                        len[i] = len[j] + 1;
+                        cnt[i] = cnt[j];
+                    }
+                }
+            }
+            if(max_len == len[i])res += cnt[i];
+            if(max_len < len[i]){
+                max_len = len[i];
+                res = cnt[i];
+            }
+        }
+        return res;
+    }
+}
