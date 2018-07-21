7d6e707eb407a384ea8a1cd02ea6f2a2c384295b
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-21 20:24:04 +0800

    Strange Printer

---
+++ Dynamic Programming/0664-Strange Printer.java
@@ -0,0 +1,22 @@
+//Solution 1 use temp string DP solution
+class Solution {
+    public int strangePrinter(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        
+        int[][] dp = new int[101][101];
+        for (int i = 0; i < n; i++) dp[i][i] = 1;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                dp[j][j + i] = i + 1;
+                for (int k = j + 1; k <= j + i; k++) {
+                    int temp = dp[j][k - 1] + dp[k][j + i];
+                    if (s.charAt(k - 1) == s.charAt(j + i)) temp--;
+                    dp[j][j + i] = Math.min(dp[j][j + i], temp);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
