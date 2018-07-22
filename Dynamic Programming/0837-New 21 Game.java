05f18eeb93565e1cf6d20a4cc1ce89d4e41588f6
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-22 22:11:41 +0800

    New 21 Game

---
+++ Dynamic Programming/0837-New 21 Game.java
@@ -0,0 +1,14 @@
+//Solution 1 climb stairs DP solution
+class Solution {
+    public double new21Game(int N, int K, int W) {
+        if (K == 0 || N >= K + W) return 1;
+        double dp[] = new double[N + 1],  Wsum = 1, res = 0;
+        dp[0] = 1;
+        for (int i = 1; i <= N; ++i) {
+            dp[i] = Wsum / W;
+            if (i < K) Wsum += dp[i]; else res += dp[i];
+            if (i - W >= 0) Wsum -= dp[i - W];//key point
+        }
+        return res;
+    }
+}
