0cb487d71ca573536204a73e90ac18fcff3eb125
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-22 12:57:06 +0800

    Number Of Corner Rectangles

---
+++ Dynamic Programming/0750-Number Of Corner Rectangles.java
@@ -0,0 +1,16 @@
+//Solution 1 tranverse DP solution
+class Solution {
+    public int countCornerRectangles(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length - 1; i++) {
+            for (int j = i + 1; j < grid.length; j++) {
+                int counter = 0;
+                for (int k = 0; k < grid[0].length; k++) {
+                    if (grid[i][k] == 1 && grid[j][k] == 1) counter++;
+                }
+                if (counter > 0) ans += counter * (counter - 1) / 2;
+            }
+        }
+        return ans;
+    }
+}
