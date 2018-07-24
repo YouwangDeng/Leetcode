eed3085a1abcce371eacbfa40ad7046055fdcc9b
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-24 19:03:13 +0800

    N-Queens II

---
+++ Backtracking/0052-N-Queens II.java
@@ -0,0 +1,48 @@
+//Solution 1 similar solution as N-Queens
+class Solution {
+    int count = 0;
+    public int totalNQueens(int n) {
+        char[][] chess = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chess[i][j] = '.';
+            }
+        }
+        solve(chess, 0);
+        return count;
+    }
+    private void solve(char[][] chess, int row) {
+        if (row == chess.length) {
+            count = count + 1;
+            return;
+        }
+        for (int col = 0; col < chess.length; col++) {
+            if (valid(chess, row, col)) {
+                chess[row][col] = 'Q';
+                solve(chess, row + 1);
+                chess[row][col] = '.';
+            }
+        }
+    }
+    private boolean valid(char[][] chess, int row, int col) {
+        // check all cols
+        for (int i = 0; i < row; i++) {
+            if (chess[i][col] == 'Q') {
+                return false;
+            }
+        }
+        //check 45 degree
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        //check 135
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
