4b4b5bba308d48ebfea9f9d911ab06a7077e6ddf
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-27 12:46:52 +0800

    Binary Watch

---
+++ Backtracking/0401-Binary Watch.java
@@ -0,0 +1,11 @@
+//Solution 1 use bitCount and String.format methods
+class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> times = new ArrayList<>();
+        for (int h=0; h<12; h++)
+            for (int m=0; m<60; m++)
+                if (Integer.bitCount(h * 64 + m) == num)
+                    times.add(String.format("%d:%02d", h, m));
+        return times;        
+    }
+}
