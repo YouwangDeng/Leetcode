275a2d14494c44422e3cbf12ab053a6ae09b95f3
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-28 21:03:35 +0800

    Maximum Depth of Binary Tree

---
+++ DFS/0104-Maximum Depth of Binary Tree.java
@@ -0,0 +1,18 @@
+//Solution 1 clear recursive solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));//key point
+    }
+}
