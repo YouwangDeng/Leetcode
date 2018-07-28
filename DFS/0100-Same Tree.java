5095ee862a19c4c307abd7f23ae18c5e6e0186ab
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-28 20:21:59 +0800

    Same Tree

---
+++ DFS/0100-Same Tree.java
@@ -0,0 +1,18 @@
+//Solution 1 simutaniously tranverse solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null) return p == q;
+        if(p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+}
