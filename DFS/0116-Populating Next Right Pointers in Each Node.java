ac14d43ff45682e910f0af16e803ed5d87e3fe26
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-29 12:44:30 +0800

    Populating Next Right Pointers in Each Node

---
+++ DFS/0116-Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,24 @@
+//Solution 1 clear recursive solution
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null || root.left == null) return;
+        connectNodes(root.left, root.right);
+    }
+    
+    public void connectNodes(TreeLinkNode node1, TreeLinkNode node2) {
+        node1.next = node2;
+        if(node1.left != null) {
+            connectNodes(node1.right, node2.left);
+            connectNodes(node1.left, node1.right);
+            connectNodes(node2.left, node2.right);
+        }
+    } 
+}
