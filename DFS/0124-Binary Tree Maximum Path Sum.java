5fb7df0ad4c78be886f9770d576a6e1ced064752
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-29 14:46:32 +0800

    Binary Tree Maximum Path Sum

---
+++ DFS/0124-Binary Tree Maximum Path Sum.java
@@ -0,0 +1,27 @@
+//Solution 1 recursive connected nodes solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int maxValue;
+    
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+    
+    private int maxPathDown(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);//key point
+        return Math.max(left, right) + node.val;
+    }
+}
