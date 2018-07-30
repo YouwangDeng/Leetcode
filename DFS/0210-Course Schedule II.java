0c26fb78ef613fb79a6f6a24374a9eccc19920d0
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-30 12:02:10 +0800

    Course Schedule II

---
+++ DFS/0210-Course Schedule II.java
@@ -0,0 +1,38 @@
+//Solution 1 clear BFS Topological sort solution
+class Solution {
+    
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] incLinkCounts = new int[numCourses];
+        List<List<Integer>> adjs = new ArrayList<>();
+        initialiseGraph(incLinkCounts, adjs, prerequisites);
+        return solveByBFS(incLinkCounts, adjs);
+    }
+    
+    private void initialiseGraph(int[] incLinkCounts, List<List<Integer>> adjs, int[][] prerequisites){
+        int n = incLinkCounts.length;
+        while (n-- > 0) adjs.add(new ArrayList<>());
+        for (int[] edge : prerequisites) {
+            incLinkCounts[edge[0]]++;
+            adjs.get(edge[1]).add(edge[0]);
+        }
+    }
+    
+    private int[] solveByBFS(int[] incLinkCounts, List<List<Integer>> adjs){
+        int[] order = new int[incLinkCounts.length];
+        Queue<Integer> toVisit = new ArrayDeque<>();
+        for (int i = 0; i < incLinkCounts.length; i++) {
+            if (incLinkCounts[i] == 0) toVisit.offer(i);
+        }
+        int visited = 0;
+        while (!toVisit.isEmpty()) {
+            int from = toVisit.poll();
+            order[visited++] = from;
+            for (int to : adjs.get(from)) {
+                incLinkCounts[to]--;
+                if (incLinkCounts[to] == 0) toVisit.offer(to);
+            }
+        }
+        return visited == incLinkCounts.length ? order : new int[0]; 
+    }
+    
+}
