b956b8bf06163041561afb50118ddf01ed6832a1
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-30 13:34:56 +0800

    Binary Tree Paths

---
+++ DFS/0257-Binary Tree Paths.java
@@ -0,0 +1,25 @@
+//Solution 1 use DFS backtracking and StringBuilder (instead of string concatenation) solution
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(res, root, sb);
+        return res;
+    }
+    
+    private void helper(List<String> res, TreeNode root, StringBuilder sb) {
+        if(root == null) {
+            return;
+        }
+        int len = sb.length();
+        sb.append(root.val);
+        if(root.left == null && root.right == null) {
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            helper(res, root.left, sb);
+            helper(res, root.right, sb);
+        }
+        sb.setLength(len);
+    }
+}
