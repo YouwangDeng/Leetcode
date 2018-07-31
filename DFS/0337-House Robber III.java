70b3d8d0e420120d6594cfdd30e4fa1e07723997
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-07-31 21:14:40 +0800

    House Robber III

---
+++ DFS/0337-House Robber III.java
@@ -0,0 +1,29 @@
+//Solution 1 use DFS and binary array
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = robSub(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] robSub(TreeNode root) {
+        if (root == null) return new int[2];
+
+        int[] left = robSub(root.left);
+        int[] right = robSub(root.right);
+        int[] res = new int[2];
+
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] + right[0];
+
+        return res;
+    }
+}
