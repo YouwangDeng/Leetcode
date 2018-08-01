193b1de893a5ab50b5ebb4bba213500924192ca9
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-01 12:55:07 +0800

    Decode String

---
+++ DFS/0394-Decode String.java
@@ -0,0 +1,24 @@
+//Solution 1 use two stack to store integer and string
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> intStack = new Stack<>();
+        Stack<StringBuilder> strStack = new Stack<>();
+        StringBuilder cur = new StringBuilder();
+        int k = 0;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if ( ch == '[') {
+                intStack.push(k);
+                strStack.push(cur);
+                cur = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder tmp = cur;
+                cur = strStack.pop();
+                for (k = intStack.pop(); k > 0; k--) cur.append(tmp);
+            } else cur.append(ch);
+        }
+        return cur.toString();
+    }
+}
