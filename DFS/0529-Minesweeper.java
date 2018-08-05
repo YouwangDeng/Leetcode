6ed6b57b34feec10ac44db358c5946e2319c13a6
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-05 12:11:30 -0700

    Minesweeper

---
+++ DFS/0529-Minesweeper.java
@@ -0,0 +1,44 @@
+//Solution 1 clear DFS solution 
+public class Solution {
+    //If click on a mine ('M'), mark it as 'X', stop further search.
+    //If click on an empty cell ('E'), depends on how many surrounding mine:
+    //2.1 Has surrounding mine(s), mark it with number of surrounding mine(s), stop further search.
+    //2.2 No surrounding mine, mark it as 'B', continue search its 8 neighbors.
+    public char[][] updateBoard(char[][] board, int[] click) {
+        int m = board.length, n = board[0].length;
+        int row = click[0], col = click[1];
+        
+        if (board[row][col] == 'M') { // Mine
+            board[row][col] = 'X';
+        }
+        else { // Empty
+            // Get number of mines first.
+            int count = 0;
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int r = row + i, c = col + j;
+                    if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                    if (board[r][c] == 'M' || board[r][c] == 'X') count++;
+                }
+            }
+            
+            if (count > 0) { // If it is not a 'B', stop further DFS.
+                board[row][col] = (char)(count + '0');
+            }
+            else { // Continue DFS to adjacent cells.
+                board[row][col] = 'B';
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (i == 0 && j == 0) continue;
+                        int r = row + i, c = col + j;
+                        if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                        if (board[r][c] == 'E') updateBoard(board, new int[] {r, c});
+                    }
+                }
+            }
+        }
+        
+        return board;
+    }
+}
