cf4b33ee67effad343eba4cf6978439aea6fb470
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-10 09:02:42 -0700

    24 Game

---
+++ DFS/0679-24 Game.java
@@ -0,0 +1,41 @@
+//Solution 1 use double backtracking solution
+class Solution {
+
+    boolean res = false;
+    final double eps = 0.001;
+
+    public boolean judgePoint24(int[] nums) {
+        List<Double> arr = new ArrayList<>();
+        for(int n: nums) arr.add((double) n);
+        helper(arr);
+        return res;
+    }
+
+    private void helper(List<Double> arr){
+        if (res) return;
+        if (arr.size() == 1){
+            if (Math.abs(arr.get(0) - 24.0) < eps) res = true;
+            return;
+        }
+        for (int i = 1; i < arr.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                List<Double> next = new ArrayList<>();
+                Double p1 = arr.get(i), p2 = arr.get(j);
+                next.addAll(Arrays.asList(p1+p2, p1-p2, p2-p1, p1*p2));
+                if (Math.abs(p2) > eps)  next.add(p1/p2);
+                if (Math.abs(p1) > eps)  next.add(p2/p1);
+                //backtracking
+                arr.remove(i);
+                arr.remove(j);
+                for (Double n: next){
+                    arr.add(n);
+                    helper(arr);
+                    arr.remove(arr.size()-1);
+                }
+                //backtracking
+                arr.add(j, p2);
+                arr.add(i, p1);
+            }
+        }
+    }
+}
