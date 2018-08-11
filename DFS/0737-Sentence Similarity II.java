d4427a28fb90b9650afbd9ea910ef1a1530a459a
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-10 21:47:13 -0700

    Sentence Similarity II

---
+++ DFS/0737-Sentence Similarity II.java
@@ -0,0 +1,21 @@
+//Solution 1 standard Union-Find solution
+class Solution {
+    public boolean areSentencesSimilarTwo(String[] a, String[] b, String[][] pairs) {
+        if (a.length != b.length) return false;
+        Map<String, String> m = new HashMap<>();
+        for (String[] p : pairs) {
+            String parent1 = find(m, p[0]), parent2 = find(m, p[1]);
+            if (!parent1.equals(parent2)) m.put(parent1, parent2);
+        }
+
+        for (int i = 0; i < a.length; i++)
+            if (!a[i].equals(b[i]) && !find(m, a[i]).equals(find(m, b[i]))) return false;
+
+        return true;
+    }
+
+    private String find(Map<String, String> m, String s) {
+        if (!m.containsKey(s)) m.put(s, s);
+        return s.equals(m.get(s)) ? s : find(m, m.get(s));
+    }
+}
