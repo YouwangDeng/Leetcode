cf0702679eb9f4f4d3cbc92a42ab7576ae19b4a8
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-11 23:31:03 -0700

    Leaf-Similar Trees

---
+++ DFS/0872-Leaf-Similar Trees.java
@@ -0,0 +1,28 @@
+//Solution 1 use stack and DFS solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> s1 = new Stack<>(), s2 = new Stack<>();
+        s1.push(root1); s2.push(root2);
+        while (!s1.empty() && !s2.empty())
+            if (dfs(s1) != dfs(s2)) return false;
+        return s1.empty() && s2.empty();
+    }
+
+    public int dfs(Stack<TreeNode> s) {
+        while (true) {
+            TreeNode node = s.pop();
+            if (node.right != null) s.push(node.right);//key point push right first
+            if (node.left != null) s.push(node.left);
+            if (node.left == null && node.right == null) return node.val;
+        }  
+    }
+}
