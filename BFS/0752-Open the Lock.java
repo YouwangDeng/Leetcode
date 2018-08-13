f96843613983c322421c7ade81b4df075c498c10
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-12 17:20:44 -0700

    Open the Lock

---
+++ BFS/0752-Open the Lock.java
@@ -0,0 +1,77 @@
+//Solution 1 basic BFS solution slower
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Queue<String> q = new LinkedList<>();
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        Set<String> visited = new HashSet<>();
+        q.offer("0000");
+        visited.add("0000");
+        int level = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size > 0) {
+                String s = q.poll();
+                if(deads.contains(s)) {
+                    size --;
+                    continue;
+                }
+                if(s.equals(target)) return level;
+                StringBuilder sb = new StringBuilder(s);
+                for(int i = 0; i < 4; i ++) {
+                    char c = sb.charAt(i);
+                    String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+                    String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
+                    if(!visited.contains(s1) && !deads.contains(s1)) {
+                        q.offer(s1);
+                        visited.add(s1);
+                    }
+                    if(!visited.contains(s2) && !deads.contains(s2)) {
+                        q.offer(s2);
+                        visited.add(s2);
+                    }
+                }
+                size --;
+            }
+            level ++;
+        }
+        return -1;
+    }
+}
+//Solution 2 use HashSet (not Queue) two ends BFS solution faster
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> begin = new HashSet<>();
+        Set<String> end = new HashSet<>();
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        begin.add("0000");
+        end.add(target);
+        int level = 0;
+        Set<String> temp;
+        while(!begin.isEmpty() && !end.isEmpty()) {
+            if (begin.size() > end.size()) {
+                temp = begin;
+                begin = end;
+                end = temp;
+            }
+            temp = new HashSet<>();
+            for(String s : begin) {
+                if(end.contains(s)) return level;
+                if(deads.contains(s)) continue;
+                deads.add(s);
+                StringBuilder sb = new StringBuilder(s);
+                for(int i = 0; i < 4; i ++) {
+                    char c = sb.charAt(i);
+                    String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+                    String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
+                    if(!deads.contains(s1))
+                        temp.add(s1);
+                    if(!deads.contains(s2))
+                        temp.add(s2);
+                }
+            }
+            level ++;
+            begin = temp;
+        }
+        return -1;
+    }
+}
