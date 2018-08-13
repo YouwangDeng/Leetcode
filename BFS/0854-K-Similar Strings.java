73cc1468ff79dd7052d7592013152417316b01c3
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-13 09:35:55 -0700

    K-Similar Strings

---
+++ BFS/0854-K-Similar Strings.java
@@ -0,0 +1,33 @@
+//Solution 1 BFS solution guarantee the min result
+class Solution {
+    public int kSimilarity(String A, String B) {
+        if (A.equals(B)) return 0;
+        Set<String> vis= new HashSet<>();
+        Queue<String> q= new LinkedList<>();
+        q.add(A);
+        vis.add(A);
+        int res=0;
+        while(!q.isEmpty()){
+            res++;
+            for (int sz=q.size(); sz>0; sz--){
+                String s= q.poll();
+                int i=0;
+                while (s.charAt(i)==B.charAt(i)) i++;
+                for (int j=i+1; j<s.length(); j++){
+                    if (s.charAt(j)==B.charAt(j) || s.charAt(i)!=B.charAt(j) ) continue;
+                    String temp= swap(s, i, j);
+                    if (temp.equals(B)) return res;
+                    if (vis.add(temp)) q.add(temp);
+                }
+            }
+        }
+        return res;
+    }
+    public String swap(String s, int i, int j){
+        char[] ca=s.toCharArray();
+        char temp=ca[i];
+        ca[i]=ca[j];
+        ca[j]=temp;
+        return new String(ca);
+    }
+}
