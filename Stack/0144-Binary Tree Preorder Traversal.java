e5d12a715e4b3bf29b2b4dbb43a3ae44f53f72f8
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-13 20:59:24 -0700

    Binary Tree Preorder Traversal

---
+++ Stack/0144-Binary Tree Preorder Traversal.java
@@ -0,0 +1,27 @@
+//Solution 1 use one Stack solution pre-order push right first
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                result.add(node.val);
+                //key point push right first
+                stack.push(node.right);
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+}
