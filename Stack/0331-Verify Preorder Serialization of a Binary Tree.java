cbb028d0dc27dc35b270edad69b134016a85e213
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-14 16:25:52 -0700

    Verify Preorder Serialization of a Binary Tree

---
+++ Stack/0331-Verify Preorder Serialization of a Binary Tree.java
@@ -0,0 +1,40 @@
+//Solution 1 stack and math solution 
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        // using a stack, scan left to right
+        // case 1: we see a number, just push it to the stack
+        // case 2: we see #, check if the top of stack is also #
+        // if so, pop #, pop the number in a while loop, until top of stack is not #
+        // if not, push it to stack
+        // in the end, check if stack size is 1, and stack top is #
+        if (preorder == null) {
+            return false;
+        }
+        Stack<String> st = new Stack<>();
+        String[] strs = preorder.split(",");
+        for (int pos = 0; pos < strs.length; pos++) {
+            String curr = strs[pos];
+            while (curr.equals("#") && !st.isEmpty() && st.peek().equals(curr)) {
+                st.pop();
+                if (st.isEmpty()) {
+                    return false;
+                }
+                st.pop();
+            }
+            st.push(curr);
+        }
+        return st.size() == 1 && st.peek().equals("#");
+    }
+}
+//Solution 2 clear math solution faster
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int diff = 1;
+        for (String node: nodes) {
+            if (--diff < 0) return false;
+            if (!node.equals("#")) diff += 2;
+        }
+        return diff == 0;
+    }
+}
