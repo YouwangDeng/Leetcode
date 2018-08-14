e0b9ed296386963238e04981c02a27a297ccb62f
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-13 22:48:11 -0700

    Binary Tree Postorder Traversal

---
+++ Stack/0145-Binary Tree Postorder Traversal.java
@@ -0,0 +1,30 @@
+//Solution 1 use one stack and addFirst method consice solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return ans;
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            ans.addFirst(cur.val);
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            } 
+        }
+        return ans;
+    }
+}
