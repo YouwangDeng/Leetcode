978fde69a64c2a25d7df2883442a79744ae70654
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-14 21:25:09 -0700

    Next Greater Element I

---
+++ Stack/0496-Next Greater Element I.java
@@ -0,0 +1,15 @@
+//Solution 1 clear one stack solution 
+class Solution {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }   
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
+    }
+}
