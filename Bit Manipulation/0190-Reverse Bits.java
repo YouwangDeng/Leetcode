2b4ce40ce5a459d00291d14083071412f32c12fa
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-16 11:22:32 -0700

    Reverse Bits

---
+++ Bit Manipulation/0190-Reverse Bits.java
@@ -0,0 +1,14 @@
+//Solution 1 unsigned shift bit manipulation solution
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;   // CATCH: must do unsigned shift
+            if (i < 31) // CATCH: for last digit, don't shift!
+                result <<= 1;
+        }
+        return result;
+    }
+}
