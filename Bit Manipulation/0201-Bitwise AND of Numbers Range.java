3441f0564ea48dbe83f339da105e9a46c5f5b259
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-16 11:51:51 -0700

    Bitwise AND of Numbers Range

---
+++ Bit Manipulation/0201-Bitwise AND of Numbers Range.java
@@ -0,0 +1,15 @@
+//Solution 1 shift manipulation solution
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        if(m == 0){
+            return 0;
+        }
+        int moveFactor = 1;
+        while(m != n){
+            m >>= 1;
+            n >>= 1;
+            moveFactor <<= 1;
+        }
+        return m * moveFactor;
+    }
+}
