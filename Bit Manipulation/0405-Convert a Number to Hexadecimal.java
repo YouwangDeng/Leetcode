aec826a177306abdf1537599b37352e07a992cbe
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-16 22:40:42 -0700

    Convert a Number to Hexadecimal

---
+++ Bit Manipulation/0405-Convert a Number to Hexadecimal.java
@@ -0,0 +1,23 @@
+//Solution 1 concise bit manipulation solution
+/*
+Basic idea: each time we take a look at the last four digits of
+            binary verion of the input, and maps that to a hex char
+            shift the input to the right by 4 bits, do it again
+            until input becomes 0.
+
+*/
+
+public class Solution {
+    
+    char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    
+    public String toHex(int num) {
+        if(num == 0) return "0";
+        String result = "";
+        while(num != 0){
+            result = map[(num & 15)] + result; 
+            num = (num >>> 4);
+        }
+        return result;
+    }
+}
