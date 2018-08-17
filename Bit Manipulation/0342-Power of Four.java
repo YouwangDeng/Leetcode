09ec016aa21719f5ab05822a0eec2df02caa3174
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-16 18:50:14 -0700

    Power of Four

---
+++ Bit Manipulation/0342-Power of Four.java
@@ -0,0 +1,8 @@
+//Solution 1 clever constant select bit manipulation solution
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
+        //0x55555555 is to get rid of those power of 2 but not power of 4
+        //so that the single 1 bit always appears at the odd position 
+    }
+}
