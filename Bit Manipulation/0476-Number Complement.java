ce481bb0b666d3386c5f0227ec5256e06cbc2c5d
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-17 17:03:13 -0700

    Number Complement

---
+++ Bit Manipulation/0476-Number Complement.java
@@ -0,0 +1,8 @@
+//Solution 1 implement mask solution
+class Solution {
+    public int findComplement(int num) {
+        int mask = 1;
+        while (mask < num) mask = (mask << 1) | 1;
+        return ~num & mask;
+    }
+}
