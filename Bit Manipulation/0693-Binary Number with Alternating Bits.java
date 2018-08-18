8abb2bbc2efbe8316a879faeb85dd7f084bf1fad
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-17 18:03:00 -0700

    Binary Number with Alternating Bits

---
+++ Bit Manipulation/0693-Binary Number with Alternating Bits.java
@@ -0,0 +1,16 @@
+//Solution 1 bit shift solution
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        /*
+        n =         1 0 1 0 1 0 1 0
+        n >> 1      0 1 0 1 0 1 0 1
+        n ^ n>>1    1 1 1 1 1 1 1 1
+        n           1 1 1 1 1 1 1 1
+        n + 1     1 0 0 0 0 0 0 0 0
+        n & (n+1)   0 0 0 0 0 0 0 0
+        */
+
+        n = n ^ (n>>1);
+        return (n & n+1) == 0;
+    }
+}
