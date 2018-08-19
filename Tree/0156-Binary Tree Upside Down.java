8e29715520d50ad13dcbce66fee7cd3ce899370a
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-18 17:24:32 -0700

    Binary Tree Upside Down

---
+++ Tree/0156-Binary Tree Upside Down.java
@@ -0,0 +1,31 @@
+//Solution 1 use cur, pre and next to to Treenode manipulation
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode curr = root;
+        TreeNode next = null;
+        TreeNode temp = null;
+        TreeNode prev = null;
+
+        while(curr != null) {
+            next = curr.left;
+
+            // swapping nodes now, need temp to keep the previous right child
+            curr.left = temp;
+            temp = curr.right;
+            curr.right = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }  
+}
