476866b1cc63b475422b7d266a4574eb83a30673
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-01 13:02:19 -0700

    Equal Tree Partition

---
+++ Tree/0663-Equal Tree Partition.java
@@ -0,0 +1,25 @@
+//Solution 1 use HashMap solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean checkEqualTree(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int sum = getsum(root, map);
+        if(sum == 0)return map.getOrDefault(sum, 0) > 1;//special case: 0, -1, 1
+        return sum%2 == 0 && map.containsKey(sum/2);
+    }
+    
+    public int getsum(TreeNode root, Map<Integer, Integer> map ){
+        if(root == null)return 0;
+        int cur = root.val + getsum(root.left, map) + getsum(root.right, map);
+        map.put(cur, map.getOrDefault(cur,0) + 1);
+        return cur;
+    }
+}
