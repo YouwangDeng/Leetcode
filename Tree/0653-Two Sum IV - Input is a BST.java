f5b22c257d1bbb6ed3a120b705685ade6f1bd991
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-31 21:38:29 -0700

    Two Sum IV - Input is a BST

---
+++ Tree/0653-Two Sum IV - Input is a BST.java
@@ -0,0 +1,24 @@
+//Solution 1 use HashSet and preorder traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return dfs(root, set, k);
+    }
+    
+    public boolean dfs(TreeNode root, HashSet<Integer> set, int k){
+        if(root == null)return false;
+        if(set.contains(k - root.val)) return true;
+        set.add(root.val);
+        return dfs(root.left, set, k) || dfs(root.right, set, k);
+    }
+}
