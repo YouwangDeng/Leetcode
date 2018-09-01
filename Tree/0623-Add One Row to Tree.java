7b55ea4079e67671fbfb49bd9e4c1b822d7b3588
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-31 19:16:30 -0700

    Add One Row to Tree

---
+++ Tree/0623-Add One Row to Tree.java
@@ -0,0 +1,28 @@
+//Solution 1 use 1 and 0 represent for two scenarios
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 0 || d == 1) {
+            TreeNode newroot = new TreeNode(v);
+            //1 for left
+            newroot.left = d == 1 ? root : null;
+            //0 for right
+            newroot.right = d == 0 ? root : null;
+            //key point
+            return newroot;
+        }
+        if (root != null && d >= 2) {
+            root.left  = addOneRow(root.left,  v, d > 2 ? d - 1 : 1);
+            root.right = addOneRow(root.right, v, d > 2 ? d - 1 : 0);
+        }
+        return root;
+    }
+}
