d21381cbb1a6e3681ae081c986f88f0f936a792a
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-01 12:30:36 -0700

    Maximum Width of Binary Tree

---
+++ Tree/0662-Maximum Width of Binary Tree.java
@@ -0,0 +1,28 @@
+//Solution 1 complete binary tree 2 * i and 2 * i + 1 rule
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 0, 1, new ArrayList<Integer>(), new ArrayList<Integer>());
+    }
+    
+    public int dfs(TreeNode root, int level, int order, List<Integer> start, List<Integer> end){
+        if(root == null)return 0;
+        if(start.size() == level){
+            start.add(order); end.add(order);
+        }
+        else end.set(level, order); //key point
+        int cur = end.get(level) - start.get(level) + 1;
+        //key point
+        int left = dfs(root.left, level + 1, 2*order, start, end);
+        int right = dfs(root.right, level + 1, 2*order + 1, start, end);
+        return Math.max(cur, Math.max(left, right));
+    }
+}
