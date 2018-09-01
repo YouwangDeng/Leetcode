e4566b517069ec1893063b5dc259e63afb463818
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-08-31 18:33:54 -0700

    N-ary Tree Preorder Traversal

---
+++ Tree/0589-N-ary Tree Preorder Traversal.java
@@ -0,0 +1,28 @@
+//Solution 1 standard preorder recursive solution 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> list = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        if (root == null)
+            return list;
+        
+        list.add(root.val);
+        for(Node node: root.children)
+            preorder(node);
+                
+        return list;
+    }
+}
