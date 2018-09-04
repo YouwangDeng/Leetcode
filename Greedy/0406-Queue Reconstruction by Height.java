5581f878767d6bed2f9faafccea8f3d4c130948b
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-04 11:41:41 -0700

    Queue Reconstruction by Height

---
+++ Greedy/0406-Queue Reconstruction by Height.java
@@ -0,0 +1,19 @@
+//Solution 1 use defined sort and insertion
+public class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        //pick up the tallest guy first
+        //when insert the next tall guy, just need to insert him into kth position
+        //repeat until all people are inserted into list
+        Arrays.sort(people,new Comparator<int[]>(){
+           @Override
+           public int compare(int[] o1, int[] o2){
+               return o1[0]!=o2[0]?-o1[0]+o2[0]:o1[1]-o2[1];
+           }
+        });
+        List<int[]> res = new LinkedList<>();
+        for(int[] cur : people){
+            res.add(cur[1],cur);       
+        }
+        return res.toArray(new int[people.length][]);
+    }
+}
