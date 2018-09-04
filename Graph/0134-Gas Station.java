5e58835111c39e6195d71349ccac05cf699bb740
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-03 22:32:27 -0700

    Gas Station

---
+++ Graph/0134-Gas Station.java
@@ -0,0 +1,23 @@
+//Solution 1 greedy find one solution
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sumGas = 0;
+        int sumCost = 0;
+        int start = 0;
+        int tank = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sumGas += gas[i];
+            sumCost += cost[i];
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        if (sumGas < sumCost) {
+            return -1;
+        } else {
+            return start;
+        }
+    }
+}
