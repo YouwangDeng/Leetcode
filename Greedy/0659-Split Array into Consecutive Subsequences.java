1d7a186e115e49260b31e494eab620a247b923d1
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-04 22:32:04 -0700

    Split Array into Consecutive Subsequences

---
+++ Greedy/0659-Split Array into Consecutive Subsequences.java
@@ -0,0 +1,22 @@
+//Solution 1 use two HashMap and calculate the frequency of values
+class Solution {
+    public boolean isPossible(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>(), appendfreq = new HashMap<>();
+        for (int i : nums) freq.put(i, freq.getOrDefault(i,0) + 1);
+        for (int i : nums) {
+            if (freq.get(i) == 0) continue;
+            else if (appendfreq.getOrDefault(i,0) > 0) {
+                appendfreq.put(i, appendfreq.get(i) - 1);
+                appendfreq.put(i+1, appendfreq.getOrDefault(i+1,0) + 1);
+            }   
+            else if (freq.getOrDefault(i+1,0) > 0 && freq.getOrDefault(i+2,0) > 0) {
+                freq.put(i+1, freq.get(i+1) - 1);
+                freq.put(i+2, freq.get(i+2) - 1);
+                appendfreq.put(i+3, appendfreq.getOrDefault(i+3,0) + 1);
+            }
+            else return false;
+            freq.put(i, freq.get(i) - 1);
+        }
+        return true;
+    }
+}
