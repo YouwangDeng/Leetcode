f7f46f6e18a3a28d48367626717a648679e50a02
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-05 22:11:23 -0700

    Boats to Save People - modified

--- Greedy/0881-Boats to Save People.java
+++ Greedy/0881-Boats to Save People.java
@@ -2,8 +2,10 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        for (int i = 0, int j = people.length - 1; i <= j; j--)
+        int i, j;
+        for (i = 0, j = people.length - 1; i <= j; j--) {
             if (people[i] + people[j] <= limit) i++;
+        }    
         return people.length - 1 - j;
     }
 }
