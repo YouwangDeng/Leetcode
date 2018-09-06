97accb7c685da2b1b0ee7bed7cd423b3b3c5b208
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-05 21:56:51 -0700

    Walking Robot Simulation

---
+++ Greedy/0874-Walking Robot Simulation.java
@@ -0,0 +1,34 @@
+//Solution 1 concise and clear solution
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> set = new HashSet<>();
+        for (int[] obs : obstacles) {
+            set.add(obs[0] + " " + obs[1]);
+        }
+        //direction array
+        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int d = 0, x = 0, y = 0, result = 0;
+        for (int c : commands) {
+            if (c == -1) {
+                d++;
+                if (d == 4) {
+                    d = 0;
+                }
+            } else if (c == -2) {
+                d--;
+                if (d == -1) {
+                    d = 3;
+                }
+            } else {
+            	//key point
+                while (c-- > 0 && !set.contains((x + dirs[d][0]) + " " + (y + dirs[d][1]))) {
+                    x += dirs[d][0];
+                    y += dirs[d][1];
+                }
+            }
+            //key point here maximum distance
+            result = Math.max(result, x * x + y * y);
+        }
+        return result;
+    }
+}
