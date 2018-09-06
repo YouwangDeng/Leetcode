c1c1c78661eb120511ea2142dadcb9ebe9fb5e65
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-05 21:25:59 -0700

    Lemonade Change

---
+++ Greedy/0860-Lemonade Change.java
@@ -0,0 +1,14 @@
+//Solution 1 keep track the number of $5 and $10 bills solution
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int i : bills) {
+            if (i == 5) five++;
+            else if (i == 10) {five--; ten++;}
+            else if (ten > 0) {ten--; five--;}
+            else five -= 3;
+            if (five < 0) return false;
+        }
+        return true;
+    }
+}
