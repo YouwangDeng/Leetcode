e9f1703845c9b51f798631531de382b1c1506716
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-08 12:06:20 -0700

    Valid Phone Numbers

---
+++ append/0193-Valid Phone Numbers.java
@@ -0,0 +1,3 @@
+//Solution 1 regex syntax
+# Read from the file file.txt and output all valid phone numbers to stdout.
+grep -e '\(^[0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}$\)' -e '\(^([0-9]\{3\})[ ]\{1\}[0-9]\{3\}-\([0-9]\{4\}\)$\)'  file.txt
