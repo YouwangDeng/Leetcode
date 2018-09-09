2cec6852d82f1868e68ff39e57ed0476c9a2dbe7
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-08 23:28:50 -0700

    Coin Change 2

---
+++ append/0518-Coin Change 2.java
@@ -0,0 +1,15 @@
+//Solution 1 standard knapsack DP solution
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        dp[0][0] = 1;
+        
+        for (int i = 1; i <= coins.length; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i-1][j] + (j >= coins[i-1] ? dp[i][j-coins[i-1]] : 0);
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
