f938d68c4e9efd90351a5f8d6e8462d7dc1178dc
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-08 17:23:18 -0700

    Transpose File

---
+++ append/0194-Transpose File.java
@@ -0,0 +1,19 @@
+//Solution 1
+# Read from the file file.txt and print its transposed content to stdout.
+#see tutorial on  http://www.grymoire.com/Unix/Awk.html
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if(NR == 1) {
+            s[i] = $i;
+        } else {
+            s[i] = s[i] " " $i;
+        }
+    }
+}
+END {
+    for (i = 1; s[i] != ""; i++) {
+        print s[i];
+    }
+}' file.txt
+
