289b7d137219d021cfd6c6d470ab8abaf438ca6e
YouwangDeng <dengyouwang@buaa.edu.cn>
2018-09-09 21:54:15 -0700

    Random Pick with Weight

---
+++ append/0528-Random Pick with Weight.java
@@ -0,0 +1,36 @@
+//Solution 1 sum array and binary search
+class Solution {
+
+    Random random;
+    int[] wSums;
+    
+    public Solution(int[] w) {
+        this.random = new Random();
+        for(int i=1; i<w.length; i++)
+            w[i] += w[i-1];
+        this.wSums = w;
+    }
+    
+    public int pickIndex() {
+        int len = wSums.length;
+        int idx = random.nextInt(wSums[len-1]) + 1;
+        int left = 0, right = len - 1;
+        // search position 
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(wSums[mid] == idx)
+                return mid;
+            else if(wSums[mid] < idx)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
